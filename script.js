1909c624033d9e689273327cbc5747cdfb29ecfa
Chlunidia Putri Kieswendha <kieswendha@gmail.com>
2023-12-13 05:31:19 +0700

    Adjusting the layout and appearance

--- script.js
+++ script.js
@@ -18,34 +18,47 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-// modal
-
 document.addEventListener('DOMContentLoaded', function () {
+    const certificateSlider = document.querySelector('.certificate-slider');
     const certificateCards = document.querySelectorAll('.certificate-card');
-    const modal = document.querySelector('.modal');
-    const overlay = document.querySelector('.overlay'); 
-    const modalContent = document.querySelector('.modal-content');
-    const closeModal = document.querySelector('.close-modal');
-    const modalImage = document.querySelector('.modal-img');
+    const modalContainer = document.querySelector('.modal-container');
+    const overlay = document.querySelector('.overlay');
+    const modalTitle = document.querySelector('.modal-title');
+    const modalDescription = document.querySelector('.modal-description');
+    const modalImg = document.querySelector('.modal-img');
 
-    certificateCards.forEach(card => {
+    // Set width of slider based on the number of cards
+    const sliderWidth = certificateCards.length * (300 + 20 * 2); // Card width + margin
+
+    certificateSlider.style.width = `${sliderWidth}px`;
+
+    certificateCards.forEach(function (card, index) {
         card.addEventListener('click', function () {
-            const imgSrc = card.querySelector('.certificate-img').src;
-            modalImage.src = imgSrc;
-            modal.style.display = 'flex';
-            overlay.style.display = 'block'; 
+            // Set modal content based on the clicked card
+            const certificateTitle = card.querySelector('.certificate-title').textContent;
+            const certificateDescription = card.querySelector('.certificate-description').textContent;
+            const certificateImgSrc = card.querySelector('.certificate-img').getAttribute('src');
+
+            modalTitle.textContent = certificateTitle;
+            modalDescription.textContent = certificateDescription;
+            modalImg.src = certificateImgSrc;
+
+            // Display the modal and overlay
+            modalContainer.style.display = 'flex';
+            overlay.style.display = 'block';
         });
     });
 
-    closeModal.addEventListener('click', function () {
-        modal.style.display = 'none';
-        overlay.style.display = 'none'; 
-    });
+    // Function to close the modal and overlay
+    window.closeCertificateModal = function () {
+        modalContainer.style.display = 'none';
+        overlay.style.display = 'none';
+    };
 
+    // Close modal and overlay when clicking outside the modal content
     window.addEventListener('click', function (event) {
         if (event.target === overlay) {
-            modal.style.display = 'none';
-            overlay.style.display = 'none'; 
+            closeCertificateModal();
         }
     });
 });
@@ -75,3 +88,42 @@ setInterval(() => {
         number.innerHTML = `${counter}%`
     }
 }, 30 );
+
+document.addEventListener('DOMContentLoaded', function () {
+    const portfolioCards = document.querySelectorAll('.portfolio-card');
+    const modalContainer = document.querySelector('.portfolio-modal-container');
+    const overlay = document.querySelector('.portfolio-overlay');
+    const modalTitle = document.querySelector('.portfolio-modal-title');
+    const modalDescription = document.querySelector('.portfolio-card-description');
+    const modalImg = document.querySelector('.portfolio-modal-img');
+
+    portfolioCards.forEach(function (card, index) {
+        card.addEventListener('click', function () {
+            // Set modal content based on the clicked card
+            const projectTitle = card.querySelector('.project-title').textContent;
+            const projectDescription = card.querySelector('.project-description').textContent;
+            const projectImgSrc = card.querySelector('.portfolio-img').getAttribute('src');
+
+            modalTitle.textContent = projectTitle;
+            modalDescription.textContent = projectDescription;
+            modalImg.src = projectImgSrc;
+
+            // Display the modal and overlay
+            modalContainer.style.display = 'flex';
+            overlay.style.display = 'block';
+        });
+    });
+
+    // Function to close the modal and overlay
+    window.closePortfolioModal = function () {
+        modalContainer.style.display = 'none';
+        overlay.style.display = 'none';
+    };
+
+    // Close modal and overlay when clicking outside the modal content
+    window.addEventListener('click', function (event) {
+        if (event.target === overlay) {
+            closePortfolioModal();
+        }
+    });
+});
