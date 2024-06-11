3f3edfbb42fbb7b1ce7b3b75abc4fba341e86826
Chlunidia Putri Kieswendha <chlunidiapk@gmail.com>
2024-06-11 22:04:30 +0700

    Delete smooth scroll

--- script.js
+++ script.js
@@ -238,10 +238,6 @@ const portfolioContent = [
     }
 ];
 
-document.addEventListener('DOMContentLoaded', function () {
-    setupSmoothScroll();
-});
-
 function loadAchievements() {
     const container = document.getElementById('home');
     const section = document.createElement('section');
@@ -268,19 +264,6 @@ function loadAchievements() {
     sliderContainer.appendChild(slider);
     section.appendChild(sliderContainer);
     container.appendChild(section);
-}
-
-function setupSmoothScroll() {
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function (e) {
-            e.preventDefault();
-            const targetId = this.getAttribute('href').substring(1);
-            const targetElement = document.getElementById(targetId);
-            if (targetElement) {
-                targetElement.scrollIntoView({ behavior: 'smooth' });
-            }
-        });
-    });
 }
 
 function loadCertificates() {
